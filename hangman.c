5abf76756db8c414c75c68725a9045eae156ae06
Jack Spence <jack.c.spence@gmail.com>
2018-07-19 21:20:56 -0400

    Added user input and validation, only 1 alphabetic character will be read in. If capital it will be converted to lower.

--- hangman.c
+++ hangman.c
@@ -1,7 +1,96 @@
 #include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+#define MAX_LENGTH 		35 /* Length of Supercalifragilisticexpialidocious + 1*/
+#define LOSS_COUNT 		5
+#define GUESS_LENGTH 	2
+
+void
+printUsage(void);
+
+int
+validateGuess(char *guess);
+
+void
+checkGuess(char *guess, char *secretWord);
 
 int 
-main(void)
+main(int argc, char *argv[])
 {
+	char secretWord[MAX_LENGTH];
+	int notWinning = 1;
+	int guessCount = 0;
+	setbuf(stdout, NULL);
+
+	if(argc == 1)
+	{
+		/* Open .hangman file and .words files */
+	}
+	else if(argc == 2)
+	{
+		/* Open file on Command Line */
+	}
+	else
+	{
+		printf("Wrong amount of arguments\n");
+		printUsage();
+	}
+
+	while(notWinning || guessCount < LOSS_COUNT)
+	{
+		char guess[GUESS_LENGTH];
+		char buff[MAX_LENGTH];
+		int garbage;
+	
+
+		printf("Please enter a letter : ");
+		fgets(buff, GUESS_LENGTH, stdin);
+		while((garbage = getchar()) != '\n');
+		strcpy(guess, buff);
+		guess[1] = 0;
+		if(validateGuess(guess))
+		{
+			printf("Valid\n");
+		}
+		else
+		{
+			printf("Invalid\n");
+		}
+		printf("%s\n", guess);
+	}
+
 	return 0;
+}
+
+void
+printUsage(void)
+{
+	printf("USAGE: ./hangman [filename]\n");
+}
+
+int
+validateGuess(char *guess)
+{
+	for(unsigned int i = 0; i < strlen(guess); i++)
+	{
+		if(isalpha(guess[i]))
+		{
+			if(isupper(guess[i]))
+			{
+				guess[i] = tolower(guess[i]);
+			}
+		}
+		else
+		{
+			return 0;
+		}
+	}
+	return 1;
+}
+
+void
+checkGuess(char *guess, char *secretWord)
+{
+	/* Check Here */
 }
